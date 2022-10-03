63c65ac36396e4cf0efc5e69d9ef9d246089ff50
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-10-03 14:13:12 -0500

    user login and register

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+/*
+    Rutas de usuarios /auth
+
+    host + /api/auth
+
+
+*/
+
+const { Router } = require("express");
+const { check } = require('express-validator');
+const router = Router();
+
+const { createUser, loginUser, renewToken } = require("../controllers/auth");
+const { fieldsValidators } = require("../middlewares/fields-validators");
+const { validatorJWT } = require("../middlewares/vadilator-jwt");
+
+router.post(
+    "/new", 
+    [ //middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+        fieldsValidators
+
+    ], 
+    createUser);
+
+router.post(
+    "/",
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6 }),
+        fieldsValidators
+    ],
+    loginUser);
+
+router.get("/renew", validatorJWT , renewToken);
+
+module.exports = router;
